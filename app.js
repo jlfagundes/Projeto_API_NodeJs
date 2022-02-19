f812ebcec4f1fb5170742d1f3d5c25dfab0488c4
Jose Luiz Fagundes <jose.fagundes@edupulses.com.br>
2022-02-18 23:13:47 -0300

    feat: tratando erros

--- app.js
+++ app.js
@@ -2,16 +2,36 @@ const express = require('express');
 
 const app = express();
 
+const morgan = require('morgan');
+
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
+app.use(morgan('dev'));
+
 app.use('/produtos', rotaProdutos);
-app.use('/pedidos', rotaPedidos)
+app.use('/pedidos', rotaPedidos);
+
+// Quando não encontra rota entra aqui
+app.use((req, res, next) => {
+    const erro = new Error('Nao encontrada');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            message: error.message
+        }
+    });
+});
 
 app.use('/', (req, res, next) => {
     res.status(200).send({
         message: "ok, deu certo"
     });
-})
+});
 
 module.exports = app
