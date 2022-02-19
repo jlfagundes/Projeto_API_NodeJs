4dbc393c4c902c1107d97a9f8bd6421918dd3e32
Jose Luiz Fagundes <jose.fagundes@edupulses.com.br>
2022-02-18 22:52:18 -0300

    fix: include routePedidos no app

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos');
 
 app.use('/produtos', rotaProdutos);
+app.use('/pedidos', rotaPedidos)
 
 app.use('/', (req, res, next) => {
     res.status(200).send({
